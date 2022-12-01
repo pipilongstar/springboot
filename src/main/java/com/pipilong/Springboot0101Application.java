940fa3866108e7822fd8e01c9d029dabae894d23
pipilong <2534158602@qq.com>
2022-12-01 09:11:32 +0800

    update

---
+++ src/main/java/com/pipilong/Springboot0101Application.java
@@ -0,0 +1,15 @@
+package com.pipilong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot0101Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Springboot0101Application.class, args);
+
+    }
+
+}
